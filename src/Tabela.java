30d08be6653164dc6736e12fcf8ca33d5300365c
Luis Henrique <henriquegundes@outlook.com>
2018-09-26 09:17:01 -0300

    Update

--- src/Tabela.java
+++ src/Tabela.java
@@ -42,6 +42,34 @@ public class Tabela
 		
 		this.marcador += incremento;
 	}
+	
+	public void declaracaoPrevia( Token t )
+	   {
+	     	if ( !this.isExiste( t.image ) )
+			{
+				System.out.println("erro: variável " + t.image + " não declarada na linha " + t.beginLine + "\n");
+			}
+	   }
+
+    public void criarVariavel( Token t, char _c_tp )
+    {
+		Simbolo simb = null;
+		switch ( _c_tp )
+		{
+			case 'N':
+				simb = new Simbolo( t.image );
+				break;
+			case 'S':
+				simb = new Simbolo( t.image, Tipo.PALAVRA );
+				break;
+		}
+
+	  	if ( !this.incluir( simb ) )
+	  	{
+			System.out.println("erro: variável " + t.image + " repetida na linha " + t.beginLine + "\n");
+	  	}
+	 
+    }  
 	   
 	public int consultarReferencia( String _chave )
 	{
